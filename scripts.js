ffa3bf04986e21dc7916438037cfacfc01ddd6eb
Göran Olson <goran@famolson.se>
2024-02-05 16:26:52 +0100

    added localStorage functionality

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
 const flagg_add_last = true;
+const TODO = 'todo-list';
 
 const btnNew = document.querySelector('.todo-container .adding button');
 btnNew.addEventListener('click', addNewPost);
 
 const todoList = document.querySelector('.todo-container .todo-list');
 todoList.addEventListener('click', listClicked);
+
+
+// Init system by importing todos from localStorage
+getStoredItems();
+
 
 function addNewPost(e) {
     e.preventDefault();
@@ -14,10 +20,57 @@ function addNewPost(e) {
     input.value = '';
     const id = todoList.children.length + 1;
 
-    if (todoText) {
+    addTodo( [todoText] );  
+
+    updateStoredItems();  // Add to localStorage
+
+}
+
+function getStoredItems() {
+    const importedList = JSON.parse( localStorage.getItem(TODO) );
+    //  console.log('importedList', importedList);
+
+    if (importedList) {
+        // 0 :['Henrik', 'done'], 1 :['Hanna']
+        for (const todoPost of importedList) {
+            // console.log(iterator);
+            addTodo(todoPost);
+        }
+    }
+}
+
+function updateStoredItems() {  // Store all post in localStorage
+    let postList = [];
+
+    for (let i = 0; i < todoList.children.length; i++) {
+        const element = todoList.children[i];
+
+        if (todoList.children[i].classList.contains('done')) {
+            postList.push( [element.children[0].innerText, 'done']);
+        }
+        else {
+            postList.push( [element.children[0].innerText]);
+        }
+    }
+
+    if (postList) {
+        localStorage.setItem(TODO, JSON.stringify(postList));
+    }
+}
+
+function addTodo(array) {
+    if (array) {
+        const txt = array[0];
+        const done = (array.includes('done')) ? 'done' : '';
+
+        const positionText = (flagg_add_last) ? 'beforeend' : 'afterbegin';  // Add first or last
+
+
+        //  id="post_${(todoList.children.length + 1)}" - NOT IN USE
+
         newPost = `
-        <article id="post_${id}" class="post">
-            <div class="chore">${todoText}</div>
+        <article class="post ${done}">
+            <div class="chore">${txt}</div>
             <div class="actions">
                 <i class='up fa fa-arrow-up'></i>
                 <i class='down fa fa-arrow-down'></i>
@@ -27,9 +80,12 @@ function addNewPost(e) {
         </article>`;
         // console.log(newPost);
 
-        const positionText = (flagg_add_last) ? 'beforeend' : 'afterbegin';  // Add first or last
+        
         todoList.insertAdjacentHTML(positionText, newPost);
+
+        // to localStorage
     }
+
 }
 
 function listClicked(e) {
@@ -61,4 +117,6 @@ function listClicked(e) {
             }
             break;
     }
+    
+    updateStoredItems(); // Update localStorage
 }
